2d6eede06316fd0793471507ac66f169d4f31ead
Joxos <xujunhao61@163.com>
2021-08-12 22:47:47 +0800

    update

--- src/Functions.h
+++ src/Functions.h
@@ -7,15 +7,5 @@ string getuntil(char sign);
 void trim(string& msg);
 void validateFolder(const char*);
 template <typename T>
-bool ifexsist(vector<T>, T);
 void assert(bool, bool);
 
-template<typename T>
-inline bool ifexsist(vector<T> items, T to_check) {
-    for (auto i = items.begin(); i != items.end(); ++i) {
-        if ((*i) == to_check) {
-            return true;
-        }
-    }
-    return false;
-}
